c20c67f4b12219ea16510ca6c60b55b01f6e0b15
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-15 20:58:49 +0100

    move everything to main document

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-from celery import Celery
 from dotenv import load_dotenv
-import os
+from os import getenv
+from celery import Celery
+from celery.schedules import crontab
+from database import get_database, iterate_collection
+from proxies import get_proxies, ProxyRotator
+from retailers.amazon import get_amazon_price
+from retailers.tradeinn import get_tradeinn_prices
+from retailers.pccomponentes import get_pccomponentes_prices
 
 
 load_dotenv()
 
-rabbit_ip = os.getenv("RABBITMQ_IP")
-rabbit_port = os.getenv("RABBITMQ_PORT")
-rabbit_user = os.getenv("RABBITMQ_USER")
-rabbit_password = os.getenv("RABBITMQ_PASSWORD")
+rabbit_ip = getenv("RABBITMQ_IP")
+rabbit_port = getenv("RABBITMQ_PORT")
+rabbit_user = getenv("RABBITMQ_USER")
+rabbit_password = getenv("RABBITMQ_PASSWORD")
 
 broker_url = f"amqp://{rabbit_user}:{rabbit_password}@{rabbit_ip}:{rabbit_port}//"
 
 app = Celery("tasks", broker=broker_url)
+
+db = get_database()
+proxies = get_proxies()
+proxy_rotator = ProxyRotator(proxies)
+
+
+@app.task
+def enqueue_urls():
+    """Fetch URLs from MongoDB and enqueue them for processing."""
+    
+    for product in iterate_collection(db, "monitored"):
+        process_product.delay(product)
+
+
+@app.task
+def process_product(product):
+    """Check product price in retailer website and store it in the database."""
+
+    url = str(product["url"])
+    if "amazon" in url: get_amazon_price(db, proxy_rotator, product)
+    elif "tradeinn" in url: get_tradeinn_prices(db, proxy_rotator, product)
+    elif "pccomponentes" in url: get_pccomponentes_prices(db, proxy_rotator, product)
+    else: print(f"Unknown retailer: {url}")
+
+
+app.conf.beat_schedule = {
+    'check-prices-hourly': {
+        'task': 'main.enqueue_urls',
+        'schedule': crontab(minute=0),
+    },
+}
